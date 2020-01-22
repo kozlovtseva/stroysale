fb0b5cac8463fc22f148a0a13cbaa4177e248eb0
Kozlovtseva <alina.lm@mail.ru>
2020-01-22 20:20:11 +0300

    style Main component

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,32 @@
 import React from "react";
 
+import StarIcon from "@material-ui/icons/Star";
+import styled from "styled-components";
+
 const Main: React.FC<{}> = () => {
-    return <div>Главная страница</div>;
+    return (
+        <>
+            <Title>Главная страница</Title>
+            <Text>
+                <StarIcon />
+                Авторизация прошла успешно
+                <StarIcon />
+            </Text>
+        </>
+    );
 };
+
+const Title = styled.h1`
+    color: #2196f3;
+    text-align: center;
+    font-size: 26px;
+    margin-top: 30px;
+`;
+const Text = styled.p`
+    color: #2196f3;
+    text-align: center;
+    font-size: 18px;
+    margin-top: 30px;
+`;
 
 export default Main;
