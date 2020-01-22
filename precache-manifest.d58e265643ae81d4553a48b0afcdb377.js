dd43dc33c9da2986f467fd99e732386892a8251e
Kozlovtseva <alina.lm@mail.ru>
2020-01-22 20:25:42 +0300

    Updates

---
+++ precache-manifest.d58e265643ae81d4553a48b0afcdb377.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0772e38d460013427c583d33cb04a682",
+    "url": "/stroysale/index.html"
+  },
+  {
+    "revision": "af0a57c642e5f4d45f7d",
+    "url": "/stroysale/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e491f121b306dfd78589",
+    "url": "/stroysale/static/js/2.d90d00a6.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/stroysale/static/js/2.d90d00a6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "af0a57c642e5f4d45f7d",
+    "url": "/stroysale/static/js/main.811f0619.chunk.js"
+  },
+  {
+    "revision": "43e131c3cfff24e0ea5d",
+    "url": "/stroysale/static/js/runtime-main.71569d08.js"
+  }
+]);
